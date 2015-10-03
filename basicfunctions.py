ea62d45fd3ed11d994ca9770a31421af852374d2
PiotrusWatson <piotruswatson@gmail.com>
2015-10-03 12:38:57 +0100

    Added debug tracking for the loops and numbers (including a sleep to check the generated list)
    Added a list of correct answers

--- basicfunctions.py
+++ basicfunctions.py
@@ -1,4 +1,4 @@
-import pygame, time, sys
+import pygame, time, sys, pyganim
 from random import randint
 pygame.init()
 w = 1000
@@ -7,21 +7,28 @@ h = 1000
 size = (w, h)
 screen = pygame.display.set_mode(size)
 c = pygame.time.Clock()
+font = pygame.font.Font("fonts/PressStart2p.ttf", 26)
+
 
 def main():
+    menu()
     imagelist = generateList()
-    levelnum = 0
+    levelnum = 1
     score = 0
     while levelnum < 10:
+        global correctlist
+        correctlist = []
         level(levelnum, imagelist)
         levelnum += 1
+        print correctlist
+        time.sleep(10)
         imagelist = generateList()
         
 def level(num1, list1):
     time = num1*0.5
     count = 0
     while count < 10:
-        screen.blit(randomCard(list1), (0,0))
+        screen.blit(randomCard(list1), (200,0))
         print count
         pygame.display.flip()
         c.tick(time)
@@ -31,7 +38,8 @@ def level(num1, list1):
 def randomCard(list1):
     randnumber = randint(0, (len(list1)-1))
     selected = list1.pop(randnumber)
-    
+    print randnumber
+    correctlist.append(randnumber)
     return selected
 
     
@@ -39,10 +47,12 @@ def generateList():
     count = 1
     imagelist = []
     while count <= 52:
-        imagelist.append(pygame.image.load("CardPics/"+str(count)+".png"))
+        imagelist.append(pygame.image.load("Cards/"+str(count)+".gif"))
         count+=1
     return imagelist
 
+def menu():
+    pass
 
 
 main()
