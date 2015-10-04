2e50b4bda59ac36c6bc98d45821f1f0dca7596cc
PiotrusWatson <piotruswatson@gmail.com>
2015-10-04 13:24:27 +0100

    fixed fonts

--- basicfunctions.py
+++ basicfunctions.py
@@ -61,8 +61,7 @@ def main():
     score = 0
 
     # initialize font; must be called after 'pygame.init()' to avoid 'Font not Initialized' error
-    myfont=pygame.font.SysFont("Courier New", 72)
-    font.set_bold(True)
+
 
     # set up high score
     hisc=open("high_score.txt","r+")
@@ -71,9 +70,9 @@ def main():
     print highscoreint
 
     # render text
-    label2= myfont.render("High Score= " +str(highscore), 1, (255,215,0))
+    label2= font.render("High Score= " +str(highscore), 1, (255,215,0))
     screen.blit(label2,(40,50))
-    label = myfont.render("Score = " + str(score), 1, (255,215,0))
+    label = font.render("Score = " + str(score), 1, (255,215,0))
     screen.blit(label, (40, 100))
 
 
@@ -164,13 +163,9 @@ def main():
 def level(num1, list1):
 
 
-    myfont=pygame.font.SysFont("Courier New", 72)
-    font.set_bold(True)
-
-
     #adding levels
     
-    label3= myfont.render("Level: " +str(num1), 1, (255,215,0))
+    label3= font.render("Level: " +str(num1), 1, (255,215,0))
     screen.blit(label3,(40,1))
 
     
@@ -292,7 +287,6 @@ def menu(rect, first):
     screen.blit(first, rect)
     pygame.display.flip()
     while 1:
-   selectsound.play()
         input1 = pygame.event.wait()
         if input1.type == pygame.KEYDOWN:
             if input1.key == pygame.K_SPACE:
